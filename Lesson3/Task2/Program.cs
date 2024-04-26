25f2678f65e373b3f5a2e72dc11015efe9aa48e2
Eldar <kastoom204@gmail.com>
2024-04-26 14:51:23 +0300

    Task2

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Вывода на экран квадратов чисел от 1 до N
+
+// void PrinSquares(int limit)
+// {
+//     int i = 0;
+//     while(i > n)
+//     {
+//         Console.WriteLine($"{i*i} ");
+//     }
+// }
+
+// PrintSquares(5);
+// PrintSquares(10);
+// PrintSquares(15);
+
+void PrintSquares(int limit)
+{
+    int i = 1;
+    while(i < limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
