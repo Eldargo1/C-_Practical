90305f4d0bfda540afedc8c438ec2d2f956456da
Eldar <kastoom204@gmail.com>
2024-05-02 18:05:32 +0300

    Task3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Считать с консоли строку, состоящую из 
+// цифр и латинских букв.
+// Сфорировать новую строку состоящую из
+// букв исходной строки.
+string GetLetter(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+
+string str = Console.ReadLine();
+string result = GetLetter(str);
+Console.WriteLine(result);
